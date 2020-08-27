8848c18edb635524e9f87920f70d193804b148cd
Alex Karakin <karakin.alexandr@gmail.com>
2020-08-27 15:18:15 +0300

    some modifications

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 install:
 	composer install
+make lint:
+	composer run-script phpcs -- --standard=PSR12 src bin
--- src/Cli.php
+++ src/Cli.php
@@ -7,7 +7,7 @@ use function cli\prompt;
 
 function run()
 {
-	line('Welcome to the Brain Game!');
-	$name = prompt('May I have your name?');
-	line("Hello, %s!", $name);
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }
