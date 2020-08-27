6fe1527735d7515c806522cf61e1542929e69a3d
Alex Karakin <karakin.alexandr@gmail.com>
2020-08-27 16:23:54 +0300

    some modifications

--- src/Cli.php
+++ src/Cli.php
@@ -6,7 +6,7 @@ use function cli\prompt;
 
 function run()
 {
-    line(' by Alexander Karakin!');
+    line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
 }
