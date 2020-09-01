8686acf9c74d3a7b49d72ad04a17893a2dcc747f
Alex Karakin <karakin.alexandr@gmail.com>
2020-09-01 16:19:57 +0300

    Some description changes

--- core/Communications.php
+++ core/Communications.php
@@ -11,6 +11,13 @@ function greetings() // greetings and asking username
     global $name; // global variable for username
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
+
+    line('Please, write number which game you want to play!');
+    line('First game - if number is even: 1');
+    line('Second game - calculating numbers: 2');
+    // line('Third game, ... : 3');
+    // line('Fourth game, ... : 4');
+    // line('Fifth game, ... : 5');
     line(" ");
     return $name;
 }
--- core/Engine.php
+++ core/Engine.php
@@ -2,7 +2,6 @@
 
 namespace php\project\lvl1\Engine;
 
-use function cli\line;
 use function cli\prompt;
 use function php\project\lvl1\Communications\greetings;
 use function php\project\lvl1\Firstgamelogic\startFirstGame;
@@ -14,34 +13,28 @@ use function php\project\lvl1\Secondgamelogic\startSecondGame;
 
 function run()
 {
-    greetings(); // call to greetings
-    line('Please, write number which game you want to play!');
-    line('First game - if number is even: 1');
-    line('Second game - calculating numbers: 2');
-    // line('Third game, ... : 3');
-    // line('Fourth game, ... : 4');
-    // line('Fifth game, ... : 5');
+    greetings(); // call to greetings in communications.php
     $gameNumber = prompt('Number');
 
     switch ($gameNumber) {
         case 1:
-            echo "You have chosen {$gameNumber}. ";
+            echo "You have chosen first game. ";
             startFirstGame();
             break;
         case 2:
-            echo "You have chosen {$gameNumber}. ";
+            echo "You have chosen second game. ";
             startSecondGame();
             break;
         case 3:
-            echo "You have chosen {$gameNumber}. ";
+            echo "You have chosen third game. ";
             //startThirdGame();
             break;
         case 4:
-            echo "You have chosen {$gameNumber}. ";
+            echo "You have chosen fourth game. ";
             //startFourthGame();
             break;
         case 5:
-            echo "You have chosen {$gameNumber}. ";
+            echo "You have chosen fifth game. ";
             //startFifthGame();
             break;
     }
