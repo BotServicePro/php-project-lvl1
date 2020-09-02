e5ca1aace574433384dd233c3a44674715144591
Alex Karakin <karakin.alexandr@gmail.com>
2020-09-02 23:30:39 +0300

    restored files

---
+++ src/Cli.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace php\project\lvl1\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function run()
+{
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
