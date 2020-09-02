ce979d4144d0aa542fba8272fe9e8393f4dfe0b3
Alex Karakin <karakin.alexandr@gmail.com>
2020-09-02 09:30:53 +0300

    added new use function for third game

--- core/Engine.php
+++ core/Engine.php
@@ -6,8 +6,8 @@ use function cli\prompt;
 use function php\project\lvl1\Communications\greetings;
 use function php\project\lvl1\Firstgamelogic\startFirstGame;
 use function php\project\lvl1\Secondgamelogic\startSecondGame;
+use function php\project\lvl1\Thirdgamelogic\startThirdGame;
 
-//use function php\project\lvl1\Thirdgamelogic\startThirdGame;
 //use function php\project\lvl1\Fourthgamelogic\startFourthGame;
 //use function php\project\lvl1\Fifthgamelogic\startFifthGame;
 
@@ -27,7 +27,7 @@ function run()
             break;
         case 3:
             echo "You have chosen third game. ";
-            //startThirdGame();
+            startThirdGame();
             break;
         case 4:
             echo "You have chosen fourth game. ";
