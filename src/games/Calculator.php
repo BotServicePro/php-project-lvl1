bf4b9902d05c3550874873fceead841b8c9907be
Alex Karakin <karakin.alexandr@gmail.com>
2020-09-09 11:07:26 +0300

    logic edited

--- src/games/Calculator.php
+++ src/games/Calculator.php
@@ -7,19 +7,16 @@ use function BrainGames\Engine\totalRounds;
 
 const CALCDESCRIPTION = "What is the result of the expression?";
 
-function countPlus($firstNumber, $secondNumber) // +
+function counter($expressionType, $firstNumber, $secondNumber)
 {
-    return $firstNumber + $secondNumber;
-}
-
-function countMinus($firstNumber, $secondNumber) // -
-{
-    return $firstNumber - $secondNumber;
-}
-
-function countMultiply($firstNumber, $secondNumber) // *
-{
-    return $firstNumber * $secondNumber;
+    switch ($expressionType) {
+        case "+":
+            return $firstNumber + $secondNumber;
+        case "-":
+            return $firstNumber - $secondNumber;
+        case "*":
+            return $firstNumber * $secondNumber;
+    }
 }
 
 function startCalculator()
@@ -28,29 +25,18 @@ function startCalculator()
     for ($i = 0; $i < totalRounds(); $i++) {
         $firstNumber = mt_rand(1, 15); // first random number
         $secondNumber = mt_rand(1, 15); // second random number
-        $expression = '';
-        $rightAnswer = '';
         $expressionsTypeArray = [
             ["-"],
             ["+"],
             ["*"],
         ];
-
-        switch (array_rand($expressionsTypeArray)) {
-            case 0:
-                $rightAnswer = countMinus($firstNumber, $secondNumber);
-                $expression = "{$firstNumber} - {$secondNumber}";
-                break;
-            case 1:
-                $rightAnswer = countPlus($firstNumber, $secondNumber);
-                $expression = "{$firstNumber} + {$secondNumber}";
-                break;
-            case 2:
-                $rightAnswer = countMultiply($firstNumber, $secondNumber);
-                $expression = "{$firstNumber} * {$secondNumber}";
-                break;
-        }
-        $expressionsAndAnswers [] =  array($expression, (string) $rightAnswer);
+        $tempExpressionType = $expressionsTypeArray
+        [array_rand($expressionsTypeArray, 1)]; // temp symbol
+        $tempExpressionType = $tempExpressionType[0];
+        $expressionsAndAnswers [] = [
+            "{$firstNumber} {$tempExpressionType} {$secondNumber}",
+            (string) counter($tempExpressionType, $firstNumber, $secondNumber)
+        ];
     }
     run(CALCDESCRIPTION, $expressionsAndAnswers);
 }
