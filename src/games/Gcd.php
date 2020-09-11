b5571f8ededcab0c50195e02c025767ce307506e
Alex Karakin <karakin.alexandr@gmail.com>
2020-09-11 22:02:09 +0300

    deleted comments, renamed variables, deleted unused if

--- src/games/Gcd.php
+++ src/games/Gcd.php
@@ -8,27 +8,22 @@ use const BrainGames\Engine\ROUNDSAMOUNT;
 
 const DESCRIPTION = 'Write the greatest common divisor of this numbers';
 
-function nodCalculator($firstNumber, $secondNumber) // nod number calculator
+function getGcd($firstNumber, $secondNumber) // nod number calculator
 {
-    if (!is_numeric($firstNumber) || !is_numeric($secondNumber)) {
-        print_r('Enter only numbers!');
-    }
     if ($secondNumber > 0) {
-        return nodCalculator($secondNumber, $firstNumber % $secondNumber);
-    } else {
-        return abs($firstNumber);
+        return getGcd($secondNumber, $firstNumber % $secondNumber);
     }
+    return abs($firstNumber);
 }
 
 function runGame()
 {
     $gameData = [];
-
     for ($i = 0; $i < ROUNDSAMOUNT; $i++) {
-        $firstNumber = mt_rand(1, 300); // first random number
-        $secondNumber = mt_rand(1, 300); // second random number
-        $rightAnswer = nodCalculator($firstNumber, $secondNumber);
-        $gameData [] = array("Numbers {$firstNumber} and {$secondNumber}", (string) $rightAnswer);
+        $firstNumber = mt_rand(1, 300);
+        $secondNumber = mt_rand(1, 300);
+        $rightAnswer = getGcd($firstNumber, $secondNumber);
+        $gameData [] = ["Question: $firstNumber $secondNumber", (string) $rightAnswer];
     }
     run(DESCRIPTION, $gameData);
 }
