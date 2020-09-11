a4ff90ffc746a0fc3ca8a4fcc713be5f239e0f20
Alex Karakin <karakin.alexandr@gmail.com>
2020-09-11 10:37:42 +0300

    changed logic

--- src/GameSwitcher.php
+++ src/GameSwitcher.php
@@ -10,30 +10,31 @@ use function BrainGames\games\Gcd\rungame as gcd;
 use function BrainGames\games\Prime\rungame as prime;
 use function BrainGames\games\Progression\rungame as progression;
 
-function gameChoese()
+function gameSwitcher()
 {
-    line('Please enter the game number you want to play!');
-    $gameNamesArray = [
-        ['Calculating numbers: 1', 1],
-        ['If number is even: 2', 2],
-        ['Greatest common divisor: 3', 3],
-        ['If number is prime: 4', 4],
-        ['Arithmetic progression: 5', 5],
-        ];
-    foreach ($gameNamesArray as [$description , $gameNumber]) {
+    line('Please choese the game you want to play:');
+    $gameNamesArray = ['Calculating numbers', 'If number is even', 'Greatest common divisor',
+        'If number is prime', 'Arithmetic progression'];
+    foreach ($gameNamesArray as $description) {
         print_r($description . "\n");
     }
-    $gameNumber = (int) prompt('Number');
+    $userAnswer = (string) prompt('Write game name');
 
-    if ($gameNumber === $gameNamesArray[0][1]) {
-        calc();
-    } elseif ($gameNumber === $gameNamesArray[1][1]) {
-        even();
-    } elseif ($gameNumber === $gameNamesArray[2][1]) {
-        gcd();
-    } elseif ($gameNumber === $gameNamesArray[3][1]) {
-        prime();
-    } elseif ($gameNumber === $gameNamesArray[4][1]) {
-        progression();
+    switch ($userAnswer) {
+        case 'Calculating numbers':
+            calc();
+            break;
+        case 'If number is even':
+            even();
+            break;
+        case 'Greatest common divisor':
+            gcd();
+            break;
+        case 'If number is prime':
+            prime();
+            break;
+        case 'Arithmetic progression':
+            progression();
+            break;
     }
 }
