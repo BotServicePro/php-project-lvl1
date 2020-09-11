55cca7868ac4b5f05d60ed874eb63a13e3a18ef7
Alex Karakin <karakin.alexandr@gmail.com>
2020-09-11 21:59:10 +0300

    ternar operator created

--- src/games/Even.php
+++ src/games/Even.php
@@ -8,19 +8,22 @@ use const BrainGames\Engine\ROUNDSAMOUNT;
 
 const DESCRIPTION =  'Answer "yes" if the number is even, otherwise answer "no".';
 
+function isPrime($numberForQuestion)
+{
+    if ($numberForQuestion % 2 === 0) {
+        return 'yes';
+    } elseif ($numberForQuestion % 2 !== 0) {
+        return 'no';
+    }
+}
+
 function runGame()
 {
     $gameData = [];
-
     for ($i = 0; $i < ROUNDSAMOUNT; $i++) {
-        $numberForQuestion = rand(1, 50); // random number
-        $rightAnswer = '';
-        if ($numberForQuestion % 2 === 0) { // if even, without remainder
-            $rightAnswer = 'yes';
-        } elseif ($numberForQuestion % 2 !== 0) {
-            $rightAnswer = 'no';
-        }
-        $gameData [] = array($numberForQuestion, (string) $rightAnswer);
+        $numberForQuestion = rand(1, 50);
+        $rightAnswer = isPrime($numberForQuestion);
+        $gameData [] = [$numberForQuestion, (string) $rightAnswer];
     }
     run(DESCRIPTION, $gameData);
 }
