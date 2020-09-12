5cb4895514f2123a2655f0047f96d835172f7f9a
Alex Karakin <karakin.alexandr@gmail.com>
2020-09-12 09:14:52 +0300

    logic changed

--- src/games/Progression.php
+++ src/games/Progression.php
@@ -8,26 +8,29 @@ use const BrainGames\Engine\ROUNDSAMOUNT;
 
 const DESCRIPTION = 'Write the missing number';
 
+
+function getNumbers($numbers, $randomIndexOfHiddenNumber)
+{
+    foreach ($numbers as $item) { // print array with hided number
+        if ($item === $numbers[$randomIndexOfHiddenNumber]) {
+            $numbersForUser .= '.. ';
+            continue;
+        }
+        $numbersForUser .= "$item ";
+    }
+}
+
 function runGame()
 {
     $gameData = [];
-
     for ($i = 0; $i < ROUNDSAMOUNT; $i++) {
         $startingNumber = mt_rand(1, 5000);
         $randomIndexOfHiddenNumber = mt_rand(0, 9); // which position will be hided
         $progressionStep = mt_rand(1, 90); // progression step
         $numbers = range($startingNumber, $startingNumber + ($progressionStep * 9), $progressionStep);
-        $numberForUser = '';
-
-        foreach ($numbers as $item) { // print array with hided number
-            if ($item === $numbers[$randomIndexOfHiddenNumber]) {
-                $numberForUser .= '.. ';
-                continue;
-            }
-            $numberForUser .= "$item ";
-        }
+        $numbersForUser = getNumbers($numbers, $randomIndexOfHiddenNumber);
         $rightAnswer = $numbers[$randomIndexOfHiddenNumber];
-        $gameData [] = ["$numberForUser", (string) $rightAnswer];
+        $gameData [] = ["$numbersForUser", (string) $rightAnswer];
     }
     run(DESCRIPTION, $gameData);
 }
