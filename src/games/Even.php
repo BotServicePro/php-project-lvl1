4954c637c6970612575aaa22d275822907933fbb
Alex Karakin <karakin.alexandr@gmail.com>
2020-10-12 16:36:40 +0300

    function renamed

--- src/games/Even.php
+++ src/games/Even.php
@@ -8,7 +8,7 @@ use const BrainGames\Engine\ROUNDS_COUNT;
 
 const DESCRIPTION =  'Answer "yes" if the number is even, otherwise answer "no".';
 
-function isPrime($number)
+function isEven($number)
 {
     return $number % 2 === 0;
 }
@@ -18,7 +18,7 @@ function runGame()
     $gameData = [];
     for ($i = 0; $i < ROUNDS_COUNT; $i++) {
         $number = mt_rand(1, 50);
-        $rightAnswer = isPrime($number) ? 'yes' : 'no';
+        $rightAnswer = isEven($number) ? 'yes' : 'no';
         $gameData[] = [$number, $rightAnswer];
     }
     run(DESCRIPTION, $gameData);
